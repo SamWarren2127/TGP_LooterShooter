7adb323b4ebbdd3612a861bcfc908a0edbbab3c0
Jay Spectre <Jay-Spectre@users.noreply.github.com>
2021-03-14 16:21:28 +0000

    level system

---
+++ Assets/Scripts/LevelSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSystem : MonoBehaviour
+{
+    int level = 1;
+    float currentEXP = 0f;
+    float maxEXP = 1f;
+    float expMultipler = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void increaseEXP(float exp)
+    {
+        currentEXP = expMultipler * exp;
+        if (currentEXP >= maxEXP)
+        {
+            increaseLevel();
+        }
+    }
+
+    void increaseLevel()
+    {
+        currentEXP -= maxEXP;
+        maxEXP += 1;
+        level += 1;
+    }
+
+    void increaseEXPMultiplerForSetTime(int time, int multipler)
+    {
+        StartCoroutine(TimeEXP(time, multipler));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator TimeEXP(int time, int multipler)
+    {
+        float curMultiper = expMultipler;
+        expMultipler = multipler;
+        yield return new WaitForSeconds(time);
+        expMultipler = 1;
+    }
+}
