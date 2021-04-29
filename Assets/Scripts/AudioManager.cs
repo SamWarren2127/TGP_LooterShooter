0d473fbcd6e91f1a2df67eaec1cb5b6f912057e0
SamWarren2127 <56883102+SamWarren2127@users.noreply.github.com>
2021-04-29 13:18:43 +0100

    Movement fix
    Made the player movement simpler as previous version wasnt working, fixed bug where player would still look around when paused. Future development will be adding: Sprinting, Crouching, and Jumping.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -15,22 +15,12 @@ public class AudioManager : MonoBehaviour
 
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
-            s.source.loop = s.loop;
         }
-    }
-
-    void Start()
-    {
-
     }
 
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
-        if (s == null)
-        {
-            Debug.LogWarning("Sound " + name + "not found");
-        }
         s.source.Play();
     }
 }
