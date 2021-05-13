d1d6f5955806d1d62d812ebb14271303770d9c6d
JoshKiddle <56304230+JoshKiddle@users.noreply.github.com>
2021-05-13 15:35:19 +0100

    Added footsteps when player is walking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,14 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float jumpForce;
     Vector3 moveDirection;
+    [HideInInspector]
+    bool isGrounded = true;
+
+    float oldPos;
+
+    public float stepRate = 0.5f;
+
+    public float stepCoolDown;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +28,8 @@ public class PlayerController : MonoBehaviour
             Debug.Log("_rigidbody is a null reference");
         }
         Cursor.lockState = CursorLockMode.Locked;
+
+        oldPos = transform.position.x;
     }
 
     // Update is called once per frame
@@ -28,5 +38,46 @@ public class PlayerController : MonoBehaviour
         moveDirection.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         moveDirection.z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         transform.Translate(moveDirection);
+
+        stepCoolDown -= Time.deltaTime;
+        if ((Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f) && stepCoolDown < 0f)
+        {
+            FindObjectOfType<AudioManager>().Play("Footstep");
+            stepCoolDown = stepRate;
+        }
+
+
+            /*if(_rigidbody.velocity.magnitude > 0)
+            {
+                //Debug.Log("Moving");
+            }
+
+            if(oldPos < transform.position.x || oldPos > transform.position.x)
+            {
+                FindObjectOfType<AudioManager>().Play("Footstep");
+                Debug.Log("Sound playing");
+            }*/
+
+            /*if(isGrounded == true)
+            {
+                FindObjectOfType<AudioManager>().Play("Footstep");
+            }*/
+        }
+
+    /*void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Ground")
+        {
+            isGrounded = true;
+            Debug.Log("Collided");
+        }
     }
+
+    void OnCollisionExit(Collision col)
+    {
+        if(col.gameObject.name == "Ground")
+        {
+            isGrounded = false;
+        }
+    }*/
 }
