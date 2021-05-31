ac610fac0f94f36be4d39965a899bdf126ef8641
Tony Wilson <57398501+TonyWilson-dev@users.noreply.github.com>
2021-05-31 14:34:46 +0100

    Inventory and Picking up items implemented

---
+++ Assets/EquippedItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquippedItem : MonoBehaviour
+{
+    public Mesh equippedObject;
+    public ItemData equippedItem;
+
+    public bool itemEquipped; 
+
+    public void SetEquipped(ItemData item, Mesh mesh)
+    {
+        itemEquipped = true;
+        equippedItem = item;
+        equippedObject = mesh;
+
+        UpdateVisuals();
+    }
+
+    private void UpdateVisuals()
+    {
+        GetComponent<MeshFilter>().mesh = equippedObject;
+    }
+
+    public void RemoveEquipped()
+    {
+        itemEquipped = false;
+        equippedItem = null;
+        equippedObject = null;
+
+        UpdateVisuals();
+    }
+}
