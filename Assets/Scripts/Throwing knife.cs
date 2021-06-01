4837279060b522f8443927387c136f6cb77c0991
Jay Spectre <Jay-Spectre@users.noreply.github.com>
2021-06-01 16:31:40 +0100

    throwing knife

---
+++ Assets/Scripts/Throwing knife.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwingknife : MonoBehaviour
+{
+    public GameObject knifeModel;
+    public Transform armLocation;
+    public float throwSpeed;
+    public float ammo;
+
+    bool throwing; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Throw()
+    {
+        GameObject knifethrown = Instantiate(knifeModel, armLocation.position, knifeModel.transform.rotation);
+        Rigidbody rigidbody = knifethrown.GetComponent<Rigidbody>();
+        rigidbody.AddForce(armLocation.forward * throwSpeed, ForceMode.Impulse);
+    }
+}
