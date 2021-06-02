ee49da3c2fa38692cabc351a4689e8805fe22210
JoshKiddle <56304230+JoshKiddle@users.noreply.github.com>
2021-06-02 14:37:01 +0100

    readded stop command audio manager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -8,6 +8,12 @@ public class AudioManager : MonoBehaviour
 
     public static AudioManager instance;
 
+    /*private int timerTest = 0;
+
+    public int delayAmount = 1;
+
+    protected float Timer;*/
+
     void Awake()
     {
         if(instance == null)
@@ -43,4 +49,29 @@ public class AudioManager : MonoBehaviour
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.Play();
     }
-}
+
+    public void Stop(string sound)
+    {
+        Sound s = Array.Find(sounds, item => item.name == sound);
+
+        s.source.volume = s.volume * (1f + UnityEngine.Random.Range(-s.volume / 2f, s.volume / 2f));
+        s.source.pitch = s.pitch * (1f + UnityEngine.Random.Range(-s.pitch / 2f, s.pitch / 2f));
+
+        s.source.Stop();
+    }
+
+    void Update()
+    {
+        /*Timer += Time.deltaTime;
+
+        if (Timer >= delayAmount)
+        {
+            Timer = 0f;
+            timerTest++;
+        }
+
+        if (timerTest > 15)
+        {
+            Stop("Ambience");
+        }*/
+    }
