06c8410fe1a7aeb817de549b7e9b3e221ba10a65
Lewis Powell <57398782+Lewis-Powell@users.noreply.github.com>
2021-06-03 16:45:05 +0100

    Wave System Implemented
    Wave system working

--- Assets/Scripts/Enemy Scripts/EnemyManager.cs
+++ Assets/Scripts/Enemy Scripts/EnemyManager.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
+    public GameObject[] EnemyVariants;
+    public GameObject[] enemySpawnPoints;
+    private int Wave;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Wave = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        enemySpawnPoints = GameObject.FindGameObjectsWithTag("EnemySpawn");
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        if(enemies.Length < 1)
+        {
+            SpawnEnemies(4 - enemies.Length);
+            Wave++;
+        }
+    }
+
+
+    private void SpawnEnemies(int x)
+    {
+        int m_x = x;
+        for(int i = 0; i < m_x; i++)
+        {
+            int rnd = Random.Range(0, 2);
+            int rnd2 = Random.Range(0, enemySpawnPoints.Length);
+            GameObject Enemy = Instantiate(EnemyVariants[rnd], enemySpawnPoints[rnd2].transform.position, enemySpawnPoints[rnd2].transform.rotation);
+            
+        }
         
     }
 }
